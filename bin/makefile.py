47354cebc6015c070257fa678dab1e60c3f5d0f5
Myriad-Dreamin <camiyoru@gmail.com>
2020-01-20 02:20:06 +0800

    update makefile

--- bin/makefile.py
+++ bin/makefile.py
@@ -3,6 +3,8 @@ import os
 import re
 import subprocess
 import sys
+import shutil
+import json
 
 made = set()
 
@@ -80,6 +82,8 @@ class Makefile:
     config_file_name = 'config.toml'
     context = dict()
 
+    compose_template_file = 'docker-compose.template.yml'
+    compose_file = 'docker-compose.yml'
     silent = False
 
     @classmethod
@@ -110,9 +114,88 @@ class Makefile:
         return match
 
     @classmethod
+    @require_cls('template')
+    def up(cls, *_):
+        pipe('docker-compose -f %s up' % (cls.compose_file))
+
+    @classmethod
+    @require_cls('template')
+    def down(cls, *_):
+        pipe('docker-compose -f %s down' % (cls.compose_file))
+
+    @classmethod
+    @require_cls('template')
+    def start(cls, *_):
+        pipe('docker-compose -f %s start' % (cls.compose_file))
+
+    @classmethod
+    @require_cls('template')
+    def stop(cls, *_):
+        pipe('docker-compose -f %s stop' % (cls.compose_file))
+
+    @classmethod
+    @require_cls('read_context')
+    def template(cls, *_):
+        with open(cls.compose_template_file) as f:
+            s = f.read().replace('{{redis-root-password}}', cls.context['redis-root-password'])
+            s = s.replace('{{mysql-root-password}}', cls.context['mysql-root-password'])
+            s = s.replace('{{conf-path}}', cls.context['conf-path'])
+            s = s.replace('{{logs-path}}', cls.context['logs-path'])
+            s = s.replace('{{data-path}}', cls.context['data-path'])
+            s = s.replace('{{goods-picture-path}}', cls.context['goods-picture-path'])
+            s = s.replace('{{needs-picture-path}}', cls.context['needs-picture-path'])
+            s = s.replace('{{goods-picture-path-target}}', cls.context['goods-picture-path-target'])
+            s = s.replace('{{needs-picture-path-target}}', cls.context['needs-picture-path-target'])
+            s = s.replace('{{node-name}}', cls.context['node-name'])
+            s = s.replace('{{instance-name}}', cls.context['instance-name'])
+            s = s.replace('{{target-port}}', str(cls.context['target-port']))
+            s = s.replace('{{config-file}}', cls.config_file)
+            s = s.replace('{{config-file-target}}', cls.config_file_target)
+            s = s.replace('{{mysql-norm-password}}', cls.context['mysql-norm-password'])
+            with open(cls.compose_file, 'w') as o:
+                o.write(s)
+
+    @classmethod
+    def read_context(cls, *_):
+        with open('.market-env.json', 'w+') as f:
+            c = f.read().encode('utf-8')
+            cls.context = json.loads('{}' if len(c) == 0 else c)
+            cls.context['node-name'] = cls.context.get('node-name', 'myriaddreamin/minimum-market-backend:alpine')
+            cls.context['instance-name'] = cls.context.get('instance-name', 'backend')
+
+            cls.context['redis-root-password'] = cls.context.get('redis-root-password', '12345678')
+            cls.context['mysql-root-password'] = cls.context.get('mysql-root-password', '12345678')
+            cls.context['mysql-norm-password'] = cls.context.get('mysql-norm-password', '12345678')
+
+            cls.context['conf-path'] = os.path.join(cls.current_path, cls.context.get('conf-path', 'testdb/conf'))
+            cls.context['logs-path'] = os.path.join(cls.current_path, cls.context.get('logs-path', 'testdb/logs'))
+            cls.context['data-path'] = os.path.join(cls.current_path, cls.context.get('data-path', 'testdb/data'))
+
+            cls.context['goods-picture-path'] =\
+                os.path.join(cls.current_path, cls.context.get('goods-picture-path', 'testdb/goods-picture'))
+            cls.context['needs-picture-path'] =\
+                os.path.join(cls.current_path, cls.context.get('needs-picture-path', 'testdb/needs-picture'))
+            cls.context['goods-picture-path-target'] =\
+                os.path.join(cls.current_path, cls.context.get('goods-picture-path-target', '/goods-picture'))
+            cls.context['needs-picture-path-target'] =\
+                os.path.join(cls.current_path, cls.context.get('needs-picture-path-target', '/needs-picture'))
+
+            cls.context['target-port'] = cls.context.get('target-port', 23335)
+
+            cls.config_file_name = cls.context.get('config-file-name', 'config.toml')
+            cls.config_file = os.path.join(cls.current_path, cls.config_file_name)
+            cls.config_file_target = os.path.join('/', cls.config_file_name)
+
+    @classmethod
+    @require_cls('read_context')
+    def clean(cls, *_):
+        shutil.rmtree('testdb')
+
+    @classmethod
     @require_cls('up')
     def all(cls, *_):
         pass
+
 
 
 if __name__ == '__main__':
